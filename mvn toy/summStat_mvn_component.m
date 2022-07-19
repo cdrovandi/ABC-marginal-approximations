a684ba68004e69b3f6aaab914b30b8ea90360795
cdrovandi <c.drovandi@qut.edu.au>
2022-07-19 16:20:06 +1000

    delete

--- mvn toy/summStat_mvn_component.m
+++
@@ -1,9 +0,0 @@
-function f = summStat_mvn_component(x, extra_args)
-    Sigma_P = extra_args.Sigma_P;
-    Sigma = extra_args.Sigma;
-    
-    mu = Sigma_P*inv(Sigma)*x;
-
-    f = mu(extra_args.p);
-    
-end
